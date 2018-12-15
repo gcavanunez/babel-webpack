754b724aa39dd7e7ab7c3a5d17908d7eb388bf18
Antony Cava <gcavanunez@gmail.com>
2018-12-15 12:45:34 -0500

    upddated the packages

--- src/app.js
+++ src/app.js
@@ -1,22 +1,31 @@
 // let test = () => console.log('caca');
 // test();
-import { person, sayHello } from './lib';
+import { person, sayHello, User } from "./lib";
 console.log(person.name);
-console.log(sayHello('Brady'));
+console.log(sayHello("Brady"));
 
-async function getCategory(){
-    // const response = await fetch('https://jsonplaceholder.typicode.com/posts'); 
-    const response = await fetch('http://nucliuz.com/php_rest_myblog/api/category/read.php'); 
-    const data = await response.json();
-    return data;
+async function getCategory() {
+  // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const response = await fetch(
+    "http://nucliuz.com/php_rest_myblog/api/category/read.php"
+  );
+  const data = await response.json();
+  return data;
 }
 
-async function getPosts(){
-    // const response = await fetch('https://jsonplaceholder.typicode.com/posts'); 
-    const response = await fetch('http://nucliuz.com/php_rest_myblog/api/post/read.php'); 
-    const data = await response.json();
-    return data;
+async function getPosts() {
+  // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const response = await fetch(
+    "http://nucliuz.com/php_rest_myblog/api/post/read.php"
+  );
+  const data = await response.json();
+  return data;
 }
 
 getPosts().then(posts => console.log(posts));
-getCategory().then(cats => console.log(cats))
+getCategory().then(cats => console.log(cats));
+
+console.log(sayHello("Tony"));
+const mango = new User("Tony", 24, "male");
+console.log(mango);
+console.log(sayHello(mango.name));
--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,18 @@
 export const person = {
-    name: 'Johny boy',
-    location: 'Lima',
-    age: 20
+  name: "Johny boy",
+  location: "Lima",
+  age: 20
+};
+export function sayHello(name) {
+  return `Hello ${name}`;
 }
-export function sayHello(name){
-    return `Hello ${name}`;
+export class User {
+  constructor(name, age, sex) {
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+  }
+  name() {
+    return this.name;
+  }
 }
