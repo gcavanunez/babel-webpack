7462181196532cb691ea0a9e941f2e670fd83523
Antony Cava <gcavanunez@gmail.com>
2018-12-15 12:02:16 -0500

    added changes to app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
-// let test = () => console.log(123);
+// let test = () => console.log('caca');
+// test();
+import { person, sayHello } from './lib';
+console.log(person.name);
+console.log(sayHello('Brady'));
 
-console.log(123);
+async function getCategory(){
+    // const response = await fetch('https://jsonplaceholder.typicode.com/posts'); 
+    const response = await fetch('http://nucliuz.com/php_rest_myblog/api/category/read.php'); 
+    const data = await response.json();
+    return data;
+}
+
+async function getPosts(){
+    // const response = await fetch('https://jsonplaceholder.typicode.com/posts'); 
+    const response = await fetch('http://nucliuz.com/php_rest_myblog/api/post/read.php'); 
+    const data = await response.json();
+    return data;
+}
+
+getPosts().then(posts => console.log(posts));
+getCategory().then(cats => console.log(cats))
---
+++ src/lib.js
@@ -0,0 +1,8 @@
+export const person = {
+    name: 'Johny boy',
+    location: 'Lima',
+    age: 20
+}
+export function sayHello(name){
+    return `Hello ${name}`;
+}
